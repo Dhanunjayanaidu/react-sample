a89a7af7cc8a656cebb63b894ac295a3ad8d545d
Prasad Chandra <prasadchandra6817@gmail.com>
2024-06-30 23:09:56 +0530

    Updates

---
+++ precache-manifest.2cb930e379be8b0b5273bedee66c047b.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed97f6e784f85d6ec582bc6f7b8fe284",
+    "url": "/react-sample/index.html"
+  },
+  {
+    "revision": "7b7f56dd53f844db113f",
+    "url": "/react-sample/static/css/main.0be33859.chunk.css"
+  },
+  {
+    "revision": "52616e86aa28f431b042",
+    "url": "/react-sample/static/js/2.1692c196.chunk.js"
+  },
+  {
+    "revision": "cca5ceb2f25139561504b80e5bc055dd",
+    "url": "/react-sample/static/js/2.1692c196.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0e6858bf2ede9d650961",
+    "url": "/react-sample/static/js/3.70ffe45c.chunk.js"
+  },
+  {
+    "revision": "7b7f56dd53f844db113f",
+    "url": "/react-sample/static/js/main.793fd229.chunk.js"
+  },
+  {
+    "revision": "76fcc1dad2bc081503e1",
+    "url": "/react-sample/static/js/runtime-main.42166c7e.js"
+  }
+]);
